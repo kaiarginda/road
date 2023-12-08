74a3635d1ec9339e489859523b3873519618b027
Your Name <your.email@example.com>
2023-12-08 11:29:47 -0800

    first commit

--- app/api/getreviews/route.jsx
+++ app/api/getreviews/route.jsx
@@ -1,6 +1,7 @@
 import Testimonial from "../../models/Testimonial";
-
+import { connectMongoDB } from "@/app/mongodb";
 export async function GET() {
+  await connectMongoDB();
   const testimonials = await Testimonial.find();
   return Response.json({ testimonials });
 }
